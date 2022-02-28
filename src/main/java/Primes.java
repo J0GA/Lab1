772fdbf59f01e3c83b6d3a324f45494738c57899
Stas <SLn00200182@gmail.com>
2022-02-28 10:39:07 +0300

    Another Commit

--- src/main/java/Primes.java
+++ src/main/java/Primes.java
@@ -1,11 +1,13 @@
 public class Primes {
     public static void main(String[] args) {
+        System.out.println("Простые числа меньше 100:");
         for(int i = 2; i < 100; i++) //Проверка чисел от 2 до 100
         {
             if(isPrime(i)){
-                System.out.println(i);
+                System.out.print(i+" ");
             }
         }
+        System.out.println();
     }
 
     public static boolean isPrime(int n){ //Определяет, является ли аргумент простым числом или нет
